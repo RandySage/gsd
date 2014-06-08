388c04f1813fd2d31029c5dd955b3496dbffcaec
Randy <randy@thinhorn.com>
2014-06-08 15:05:11 -0700

    Possibly implementing crude initial email capability (hacked in!) - unsure due to various changes done a week or two ago, or more

--- email_helper.py
+++ email_helper.py
@@ -1,17 +1,77 @@
+#! ./venv/bin/python
+
+import imaplib
 import json
+import re
+import time
+import email
+#from email.message import Message
+from email.mime.text import MIMEText
+
+debug_on = False
+
 with open('settings.json') as json_file:
-    settings = json.load(json_file)
+    s = json.load(json_file)
+
+def getFoldersList(obj):
+    t,folder_list = obj.list()
+    
+    folders_list = []
+    for i in range(0,len(folder_list)):
+        results = re.findall( (re.escape(s['folder_prefix']) +
+                               '''[^'"]*'''),
+                              folder_list[i])
+        if len(results):
+            folder_name = results[0].replace(s['folder_prefix'],'')
+            folders_list.append(folder_name)
+        if len(results) > 1:
+            print 'WARNING: disregarding results after first...'
+            print results[1:]
+    return folders_list
+
+def createMessage(folder, subject_text, body_text):
+    if len(body_text) == 0:
+        body_text = ' '
+    if debug_on:
+        print "Folder: %s" % folder
+        print "Subject: %s" % subject_text
+        print "-----Body----- \n%s" % body_text
+
+    obj = imaplib.IMAP4_SSL(s['secure_mail_server'],'993')
+    obj.login(s['username'],s['password'])
+
+    if len(folder) == 0:
+        full_name = s['folder_prefix'][:-1] # Chop dot from INBOX.
+    elif folder in getFoldersList(obj):
+        full_name = s['folder_prefix'] + folder
+        obj.select(full_name)
+        print 'Selected folder: %s' % full_name
+    else:
+        raise ValueError('Error: folder name (%s) not in folder list' % folder)
+    flags = ''
+
+    msg = MIMEText(body_text)
+    msg['Subject'] = subject_text
+    msg['From'] = s['sender']
+    msg['To'] = s['address']
+
+    obj.append(full_name, 
+               flags,
+               imaplib.Time2Internaldate(time.time()),
+               msg.as_string())
+
+def issue_on_server(issue_id):
+    obj = imaplib.IMAP4_SSL(s['secure_mail_server'],'993')
+    obj.login(s['username'],s['password'])
+    obj.select()
+    p,res = obj.search(None, 'BODY', issue_id)
+    return len(res[0])>0
 
 def getDataFromEmail():
-
-    import imaplib
-    import re
-
-    from time import time
-    prev = time()
+    prev = time.time()
     
-    obj = imaplib.IMAP4_SSL(settings['secure_mail_server'],'993')
-    obj.login(settings['username'],settings['password'])
+    obj = imaplib.IMAP4_SSL(s['secure_mail_server'],'993')
+    obj.login(s['username'],s['password'])
     obj.select()
 
     print obj.list
@@ -19,7 +79,7 @@ def getDataFromEmail():
     searchStr = '['
     results = obj.search(None,'SUBJECT',searchStr)
     if results[0] == 'OK':
-        msg = obj.fetch(results[1][0],"(UID BODY[TEXT])")
+        msg = obj.fetch(results[1][0],'(UID BODY[TEXT])')
         if msg[0] == 'OK':
             #import pdb; pdb.set_trace()
             msgBody = msg[1][0][1]
@@ -38,12 +98,12 @@ def getDataFromEmail():
     #  up in the file until the flush() or close() method is called.
     outFile.close()
 
-    print 'elapsed time = ' + str(time()-prev)
+    print 'elapsed time = ' + str(time.time()-prev)
         
 
 def sendEmail(message, 
               subject='System generated email from gsd',
-              recipient = settings['address'] # Default to self
+              recipient = s['address'] # Default to self
               ):
     # Import smtplib for the actual sending function
     import smtplib
@@ -54,7 +114,7 @@ def sendEmail(message,
     mime_message = MIMEText(message)
 
 
-    me = settings['address']
+    me = s['address']
 
     mime_message['Subject'] = subject
     mime_message['From'] = me
@@ -62,10 +122,33 @@ def sendEmail(message,
 
     # Send the message via our own SMTP server, but don't include the
     # envelope header.
-    s = smtplib.SMTP_SSL(settings['secure_mail_server'],'465')
-    s.login(settings['username'],settings['password'])
+    s = smtplib.SMTP_SSL(s['secure_mail_server'],'465')
+    s.login(s['username'],s['password'])
 
     s.sendmail(me, [recipient], mime_message.as_string())
 
     s.quit()
 
+if __name__ == '__main__':
+    import sys
+    import getpass
+
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        username = 'ransage'
+
+    print 'About to create message in folder Test'
+    createMessage('Test','Test message subject','test message body\nwith multiple\lines')
+
+    print 'About to create message in inbox'
+    createMessage('','Inbox test message','test inbox message body\nwith multiple\lines')
+
+    print '\nThis should raise...'
+    try:
+        createMessage('aoeu',[],[])
+    except ValueError as e:
+        print 'CAUGHT!  ',
+        print e
+
+    print '------\nDone!'
--- gsd.py
+++ gsd.py
@@ -9,14 +9,39 @@ import getpass
 from github3 import login
 import sys
 from thirdparty.query_yes_no import query_yes_no
-
+from email_helper import createMessage, issue_on_server
+import shelve
+import os.path
+import re
 
 import json
+import email_helper
+
 with open('settings.json') as json_file:
-    settings = json.load(json_file)
+    s = json.load(json_file)
+
+shelf_filename = os.path.expanduser(s['config_filename'])
+
+shelf = shelve.open(shelf_filename)
+if shelf.has_key('app_running') and shelf['app_running']:
+    print 'Application has detected a messy shutdown; manual action required'
+    print 'Quitting (TODO - implement clean-up)'
+    sys.exit(-1)
+
+shelf['app_running'] = True
+# if shelf.has_key('max_id_num'):
+#     print '0 - Key exists: %d' % shelf['max_id_num']
+  
+
+shelf.close() # TODO - find better way to write contents
+shelf = shelve.open(shelf_filename)
+if not shelf.has_key('max_id_num'):
+    shelf['max_id_num'] = -1
+    print '1 - Resetting key to %d' % shelf['max_id_num']
 
 # GENERAL LIST
 # TODO - clean up import locations & syntax
+# TODO - clean up shelve/shelf implementation
 
 
 # Server still on Python 2.6.x - defer...
@@ -159,17 +184,70 @@ def display_formatted_issue(issue):
            )
           )
 
+def create_imap_issue(issue):
+    body_codes_sep = '\n========================\n'
+
+    repo_key = '/'.join(issue.repository)
+    abbrev = s['repo_abbrevs'][repo_key]
+    subject_text = abbrev + ': ' + issue.title
+    print 'Repo key: %s' % repo_key
+    body_text = (issue.body +
+                 body_codes_sep + 
+                 ("[GIT_LABELS:%s]  " % str(map(str,issue.labels))) +
+                 ("[GIT_REPO:%s]  " % repo_key) +
+                 ("[GIT_MILESTONE:%s]  " % issue.milestone) +
+                 ("[GIT_ISSUE_NUMBER:%s]  " % issue.number) +
+                 "[DEBUG]  [DELETE]  "
+                 )
+    createMessage('', subject_text, body_text)
+                 
+def getIssueId(issue, add_if_missing=True):
+    reg_expr = ( re.escape(s['key_prefix']) +
+                 ( '[0-9]' * int(s['key_num_digits']) ))
+    #print reg_expr ,
+    results = re.findall( reg_expr, issue.body)
+    id = []
+    if len(results):
+        id = results[0]
+
+    if len(results) > 1:
+        print ( 'WARNING: disregarding results after first for issue: %s %d %s...' %
+                (str(issue.repository), issue.number, issue.title) )
+        print results[1:]
+
+    if len(results) == 0 and add_if_missing:
+        prev_id_num = shelf['max_id_num']
+        this_id_num = prev_id_num + 1
+        shelf['max_id_num'] = this_id_num
+        id = ( s['L_brac'] +
+               s['key_prefix'] +
+               ( '{:04d}'.format(int(shelf['max_id_num'])) ) +
+               s['R_brac'] )
+        new_body_text = issue.body + '  ' + id
+        if not issue.edit(body=new_body_text):
+            print('Failed to edit so not closing issue: "%s"' % str(issue))
+            raise IOError("Error: failed to write id, %s" % id)
+    print ('Id: %s' % id) ,
+    return id
+
+def issueInImap(issue_id):
+    return issue_on_server(issue_id)
+
 def list_issues(requested_state=None):
     if not requested_state in ('open','closed'):
         raise ValueException("Function only allows 'open' or 'closed'")
     # List in the order of active repositories
-    for rep_path in settings['active_repos']:
+    for rep_path in s['active_repos']:
         print("=====================%s====================="%rep_path)
         user, rep_name = rep_path.split('/')
         rep = gh.repository(user, rep_name)
         for issue in rep.iter_issues(assignee=username,
                                      state=requested_state):
+            issue_id = getIssueId(issue)
             display_formatted_issue(issue)
+            if ( requested_state == 'open' and
+                 issueInImap(issue_id) == False ):
+                create_imap_issue(issue)
 
 def create_body_string(text_to_insert):
     body_string_preface = '''WARNING: python auto-generated this text and it is likely to get edited and/or deleted.
@@ -297,6 +375,10 @@ if __name__ == "__main__":
 
         elif cmd[0] in ('exit','x','quit','q'):
             # Ends loop execution, exits program
+            # TODO - implement atexit callback to clean up
+            shelf['app_running'] = False
+            print 'Shelf max id: %d' % shelf['max_id_num']
+            shelf.close()
             break 
         
         elif cmd[0] in ('help', 'h'):
